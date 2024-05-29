4f98e60cdb2620465b5ff2dbbfdc83fa32346bc6
nazim-uddin-limon <developernazim.dev@gmail.com>
2024-05-29 09:27:46 +0600

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,6 @@
+const Page = () => {
+    return <h1>Customers page</h1>;
+  };
+  
+  export default Page;
+  
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,6 @@
+const Page = () => {
+    return <h1>Invoice Page</h1>;
+  };
+  
+  export default Page;
+  
